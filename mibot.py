46f5ef72e30e340919e23b1e03fd1c1e428e79b1
mendezkeneth29-bit <mendezkeneth29@gmail.com>
2026-04-14 17:49:24 -0600

    Update mibot.py

--- mibot.py
+++ mibot.py
@@ -471,4 +471,17 @@ async def controls(i: discord.Interaction):
 # RUN
 # -------------------------
 load_data()
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot activo"
+
+def run_web():
+    app.run(host="0.0.0.0", port=10000)
+
+threading.Thread(target=run_web).start()
 bot.run(TOKEN)
