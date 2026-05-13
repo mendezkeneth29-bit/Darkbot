dd20dc32dcd01718899707d3b266a73864943cea
mendezkeneth29-bit <mendezkeneth29@gmail.com>
2026-05-13 15:23:37 -0600

    Update mibot.py

--- mibot.py
+++ mibot.py
@@ -7,7 +7,7 @@ genai.configure(
     api_key=os.getenv("GEMINI_API_KEY")
 )
 
-model = genai.GenerativeModel("gemini-3.0-flash")
+model = genai.GenerativeModel("gemini-3-flash")
 
 from discord.ext import commands
 from discord import app_commands
